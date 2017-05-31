7ce8428aa6f03363ddfc64e284ad914c4e1e1c8f
Christian Pfitzner <christian.pfitzner@th-nuernberg.de>
2017-05-31 11:41:25 +0200

    -- modified kalman tutorial

--- kalman/kalman_intro_template.m
+++ kalman/kalman_intro_template.m
@@ -1,5 +1,4 @@
 clear all;
-close all; 
 
 % Anzahl Messwerte
 vals    = 100;
@@ -7,7 +6,7 @@ vals    = 100;
 % Streuung der Messwerte
 sigma = 5.0;
 
-% Zustandsübergangsmodel
+% ZustandsÃ¼bergangsmodel
 dT = 1.0;
 A = [1    0    dT   0;
 	0    1    0     dT;
@@ -21,7 +20,7 @@ x = [0 0 1 1]';
 data = generate_noisy_data(A, x, vals, sigma);
 plot(data(:,1),data(:,2), 'x');
 
-% Schätzung des Anfangszustands
+% SchÃ¤tzung des Anfangszustands
 x = [0 0 0 0]';
 
 % Nur die Position ist messbar, nicht die Geschwindigkeit
@@ -31,31 +30,13 @@ H = [1 0 0 0; 0 1 0 0];
 R = [sigma 0; 0 sigma];
 
 % Systemrauschen
-q = 0.0001; 
-Q = [0 0 0 0; 0 0 0 0; 0 0 q 0; 0 0 0 q];
+Q = [0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
 
 % Kovarianz
 P = eye(4);
 
-
-
-% Matrix transponieren:  A' oder transpose(A)
-
-% Matrix invertieren: inv(A)
-
 % Implementieren Sie hier den Kalmanfilter
-for i=1:vals
-  x_prio = A *x; 
-  y = H * x_prio; 
-  P = A * P * A' + Q; 
-  K = P * H' * inv(H*P*H' + R); 
-  x = x_prio + K * (data(i,:)' - y); 
-  P = (eye(4) - K * H) * P; 
-  data_kalman(i,:) = x; 
-end
-
-
 
 hold on;
-plot(data_kalman(:,1),data_kalman(:,2), 'r');
+%plot(data_kalman(:,1),data_kalman(:,2), 'r');
 hold off;
